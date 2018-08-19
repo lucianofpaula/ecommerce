2081dcc26bab8666cb6fd8c609f77e0fde78d755
FnxCode <lucianofpaula@gmail.com>
2018-08-18 21:57:22 -0300

    Autoload hcodebr classes

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-ok
+<?php   
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+$app->get('/', function(){
+    $sql = new DB\sql();
+    $results = $sql->select("SELECT * FROM tb_users");
+
+    echo json_encode($results);
+});
+
+$app->run();
+?>
